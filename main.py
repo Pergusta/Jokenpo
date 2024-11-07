d8df347f456d9c8cd2c5720c93984828c76c6b0e
Pergusta <gustavopereira.peres11037@gmail.com>
2024-11-07 01:13:15 -0300

    Função "REGRAS" e centralização da janela independente do monitor

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-## Importação das bibliotecas necessárias
+ ## Importação das bibliotecas necessárias
 from tkinter import *
 import tkinter as tk
 from random import randint 
@@ -11,9 +11,21 @@ backup = ["Pedra", "Papel", "Tesoura", "Lagarto", "Spock!"]
 computador = randint(0,4)
 player = -1
 start = False
+janela_lar = 700
+janela_alt = 600
 
 
 ## Funções
+
+# Criação de duas telas diferentes na mesma janela
+def voltartela1():
+    tela2.pack_forget()
+    tela1.pack()
+
+def irtela2():
+    tela1.pack_forget()
+    tela2.pack()
+
 
 # Coisinha pra fazer o jokenpo ir de um em um ficar bonitao
 def jokenpo():
@@ -80,27 +92,35 @@ def boraconferi(player, computador):
 
 ## Janela
 
-# Definições da janela
+# Definições da janela (faz ela ter duas telas, renomeia, tamanho inalteravel e deixa centralizada em qualquer monitor)
 janela = Tk()
+tela1 = Frame(janela)
+tela2 = Frame(janela)
 janela.title("Pedra, Papel, Tesoura, Lagarto, Spock!")
-janela.minsize(500,500)
-janela.eval('tk::PlaceWindow . center')
+janela.resizable(False,False)
+lar_monitor = janela.winfo_screenwidth()
+alt_monitor = janela.winfo_screenheight()
+pos_x = (lar_monitor // 2) - (janela_lar // 2)
+pos_y = (alt_monitor // 2) - (janela_alt // 2)
+janela.geometry(f"{janela_lar}x{janela_alt}+{pos_x}+{pos_y}")
 
-# Coisas que tem na janela
-botao1 = Button(janela, text='Pedra', width=15, command=lambda: opicao(0))
-botao2 = Button(janela, text='Papel', width=15, command=lambda: opicao(1))
-botao3 = Button(janela, text='Tesoura', width=15, command=lambda: opicao(2))
-botao4 = Button(janela, text='Lagarto', width=15, command=lambda: opicao(3))
-botao5 = Button(janela, text='Spock', width=15, command=lambda: opicao(4))
+# Coisas que tem na tela 1
+irajuda = Button(tela1, text="REGRAS", command=irtela2)
+botao1 = Button(tela1, text='Pedra', width=15, command=lambda: opicao(0))
+botao2 = Button(tela1, text='Papel', width=15, command=lambda: opicao(1))
+botao3 = Button(tela1, text='Tesoura', width=15, command=lambda: opicao(2))
+botao4 = Button(tela1, text='Lagarto', width=15, command=lambda: opicao(3))
+botao5 = Button(tela1, text='Spock', width=15, command=lambda: opicao(4))
 
-preparo = Label(janela, text='', font=("Arial", 20))
-p = Label(janela,text='FAÇA SUA ESCOLHA', font=("Arial", 20))
-jogada_player = Label(janela, text='', font=("Arial", 15))
-jogada_computador = Label(janela, text='', font=("Arial", 15))
-resultado = Label(janela, text='', font=("Arial", 15))
+preparo = Label(tela1, text='', font=("Arial", 20))
+p = Label(tela1,text='FAÇA SUA ESCOLHA', font=("Arial", 20))
+jogada_player = Label(tela1, text='', font=("Arial", 15))
+jogada_computador = Label(tela1, text='', font=("Arial", 15))
+resultado = Label(tela1, text='', font=("Arial", 15))
 
 # Posicionamento das coisas que tem na janela
-p.pack(pady=40)
+p.pack(pady=30)
+irajuda.pack(pady=20)
 botao1.pack(pady=2)
 botao2.pack(pady=2)
 botao3.pack(pady=2)
@@ -111,6 +131,16 @@ jogada_player.pack()
 jogada_computador.pack()
 resultado.pack()
 
+# Coisas da tela 2
+btnvoltar = Button(tela2, text="Voltar", command=voltartela1)
+
+# Processo para ler e converter a imagem das regras para que o tk consiga mostrar
+img_regras = Image.open(r"C:\Codes\Jokenpo\imagem\regras.png")
+img_regras_tk = ImageTk.PhotoImage(img_regras)
+label_img = Label(tela2, image=img_regras_tk)
+label_img.pack(pady=20)
+btnvoltar.pack(pady=10)
 
 ## Execução da janela
+tela1.pack()
 janela.mainloop()
